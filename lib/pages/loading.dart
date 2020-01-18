814579a916e5088f0b74e5cd06918e4eb151fb6c
Mohamed Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-01-18 18:09:56 +0100

    Displaying the time and the location on the home screen after getting the data from the loading screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,18 +8,17 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
   //void getData() async {
-     //simulate network request for a username and its bio
-    // String username = await Future.delayed(Duration(seconds: 3), () {
-    //   return 'yoshi';
-    // });
+  //simulate network request for a username and its bio
+  // String username = await Future.delayed(Duration(seconds: 3), () {
+  //   return 'yoshi';
+  // });
 
-    // String bio = await Future.delayed(Duration(seconds: 2), () {
-    //   return 'music, judo & gamer' ;
-    // });
+  // String bio = await Future.delayed(Duration(seconds: 2), () {
+  //   return 'music, judo & gamer' ;
+  // });
 
-    // print('$username - $bio');
+  // print('$username - $bio');
 
   // testing the http package
   //   Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
@@ -28,18 +27,25 @@ class _LoadingState extends State<Loading> {
   //   print(data['title']);
   // }
 
-
   void setupWorldTime() async {
-    WorldTime instance = WorldTime(flag: 'algeria.png',location: 'Algiers',url: 'Africa/Algiers');
+    WorldTime instance = WorldTime(
+      flag: 'algeria.png',
+      location: 'Algiers',
+      url: 'Africa/Algiers',
+    );
     await instance.getTime();
     print(instance.time);
-    Navigator.pushReplacementNamed(context, '/home', arguments: {
-      'location': instance.location,
-      'flag' : instance.flag,
-      'time' : instance.time
-    });
+    Navigator.pushReplacementNamed(
+      context,
+      '/home',
+      arguments: {
+        'location': instance.location,
+        'flag': instance.flag,
+        'time': instance.time,
+      },
+    );
   }
-  
+
   @override
   void initState() {
     super.initState();
@@ -53,7 +59,7 @@ class _LoadingState extends State<Loading> {
       body: Padding(
         padding: EdgeInsets.all(50.0),
         child: Text('Loading'),
-      )
+      ),
     );
   }
 }
