15f2c9de1fa1a53a2d5dc18e087b072524a8c4e8
Mohamed Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-01-18 01:18:49 +0100

    Getting world time of Algiers and adjusting it according to offset

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,8 +9,8 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    // //simulate network request for a username
+  //void getData() async {
+     //simulate network request for a username and its bio
     // String username = await Future.delayed(Duration(seconds: 3), () {
     //   return 'yoshi';
     // });
@@ -21,16 +21,35 @@ class _LoadingState extends State<Loading> {
 
     // print('$username - $bio');
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
-  }
+  // testing the http package
+  //   Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+  //   Map data = jsonDecode(response.body);
+  //   print(data);
+  //   print(data['title']);
+  // }
 
+  void getTime() async {
+    // make the request 
+    Response response = await get('http://worldtimeapi.org/api/timezone/Africa/Algiers');
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    // get properties from data
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(dateTime);
+    //print(offset);
+    
+    // create a DateTime object
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+  
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
     print('Hey there ! I\'m testing asynchronous programming !');
   }
 
